85d1d971f379b648f6ee95da8e42c38dc02b79b9
iamharshdev <nitingoswami473@gmail.com>
2020-08-14 12:10:03 +0530

    added layout css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+const { parallel, src, dest } = require('gulp');
+
+function javascript() {
+	return src('src/js/*.js').pipe(dest('dist/'));
 }
 
-exports.default = defaultTask
+function css() {
+	return gulp
+		.src('./src/sass/**/*.scss')
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('./dist'));
+}
+
+function minifyCss() {
+	return gulp
+		.src('./src/sass/**/*.scss')
+		.pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(gulp.dest('./dist/minified/'));
+}
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+exports.build = parallel(javascript, css, minifyCss);
